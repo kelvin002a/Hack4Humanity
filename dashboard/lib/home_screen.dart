61d90b802dd8747791d54ea328740ecf8fe8598d
KELVIN P ANEESH <55386051+kelvin002a@users.noreply.github.com>
2021-09-17 18:55:29 +0530

    dashboard added

---
+++ dashboard/lib/home_screen.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // to get size
+    var size = MediaQuery.of(context).size;
+
+    // style
+    var cardTextStyle = TextStyle(
+        fontFamily: "Montserrat Regular",
+        fontSize: 14,
+        color: Color.fromRGBO(63, 63, 63, 1));
+
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: size.height * .3,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  alignment: Alignment.topCenter,
+                  image: AssetImage('assets/images/top_header.png')),
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    height: 64,
+                    margin: EdgeInsets.only(bottom: 20),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 32,
+                          backgroundImage: NetworkImage(
+                              'https://i.pinimg.com/originals/78/07/03/78070395106fcd1c3e66e3b3810568bb.jpg'),
+                        ),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              'KTU STUDENT',
+                              style: TextStyle(
+                                  fontFamily: "Montserrat Medium",
+                                  color: Colors.white,
+                                  fontSize: 20),
+                            ),
+                            Text(
+                              'KTU18CS018',
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.white,
+                                  fontFamily: "Montserrat Regular"),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: GridView.count(
+                      mainAxisSpacing: 10,
+                      crossAxisSpacing: 10,
+                      primary: false,
+                      crossAxisCount: 2,
+                      children: <Widget>[
+                        Card(
+                          shape:RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8)
+                          ),
+                          elevation: 4,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              SvgPicture.network(
+                                'https://image.flaticon.com/icons/svg/1904/1904425.svg',
+                                height: 128,
+                              ),
+                              Text(
+                                'Personal Info',
+                                style: cardTextStyle,
+                              )
+                            ],
+                          ),
+                        ),
+
+                        Card(
+                          shape:RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8)
+                          ),
+                          elevation: 4,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              SvgPicture.network(
+                                'https://image.flaticon.com/icons/svg/1904/1904565.svg',
+                                height: 128,
+                              ),
+                              Text(
+                                'Course Schedule',
+                                style: cardTextStyle,
+                              )
+                            ],
+                          ),
+                        ),
+
+                        Card(
+                          shape:RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8)
+                          ),
+                          elevation: 4,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              SvgPicture.network(
+                                'https://image.flaticon.com/icons/svg/1904/1904527.svg',
+                                height: 128,
+                              ),
+                              Text(
+                                'Notes',
+                                style: cardTextStyle,
+                              )
+                            ],
+                          ),
+                        ),
+
+                        Card(
+                          shape:RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8)
+                          ),
+                          elevation: 4,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              SvgPicture.network(
+                                'https://image.flaticon.com/icons/svg/1904/1904437.svg',
+                                height: 128,
+                              ),
+                              Text(
+                                'Study Result',
+                                style: cardTextStyle,
+                              )
+                            ],
+                          ),
+                        ),
+
+                        Card(
+                          shape:RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8)
+                          ),
+                          elevation: 4,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              SvgPicture.network(
+                                'https://image.flaticon.com/icons/svg/1904/1904235.svg',
+                                height: 128,
+                              ),
+                              Text(
+                                'Q&A',
+                                style: cardTextStyle,
+                              )
+                            ],
+                          ),
+                        ),
+
+                        Card(
+                          shape:RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8)
+                          ),
+                          elevation: 4,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              SvgPicture.network(
+                                'https://image.flaticon.com/icons/svg/1904/1904221.svg',
+                                height: 128,
+                              ),
+                              Text(
+                                'Internship',
+                                style: cardTextStyle,
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
